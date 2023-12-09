b00be60fff0b0c167426aabb70178474cd766794
artrugh <arturo.rugh@gmail.com>
2023-12-09 20:27:58 +0100

    chore: readability

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,54 @@
 import "./App.css";
-import React, { useEffect } from "react";
-import { Products } from "./components/Products.component";
+import React, { useEffect, useState } from "react";
+import { ProductList } from "./components/Products.component";
 import { Home } from "./components/Home.component";
 import data from "./db/db.json";
+import { Product } from "./model/model";
 
 const App: React.FC = () => {
-    useEffect(() => {
-        fetch("https://jsonplaceholder.typicode.com/posts", {
-            method: "POST",
-            body: JSON.stringify(data),
-            headers: {
-                "Content-type": "application/json; charset=UTF-8",
-            },
-        })
-            .then((response) => {
-                if (!response.ok) {
-                    throw new Error("Failed to post data");
-                }
-                return response.json();
-            })
-            .then((json) => console.log("data posted:", json))
-            .catch((error) => console.error("Error posting data:", error));
-    }, []);
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/posts", {
+      method: "POST",
+      body: JSON.stringify(data),
+      headers: {
+        "Content-type": "application/json; charset=UTF-8",
+      },
+    })
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Failed to post data");
+        }
+        return response.json();
+      })
+      .then((json) => console.log("data posted:", json))
+      .catch((error) => console.error("Error posting data:", error));
+  }, []);
 
-    return (
-        <>
-            <h2>Click the button to see the products</h2>
-            <button>to the products page</button>
-            <Home />
-            <Products />
-        </>
-    );
+  const [products, setProducts] = useState<Product[]>([]);
+
+  useEffect(() => {
+    fetch("http://localhost:8089/products")
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Failed to fetch data");
+        }
+        console.log("response.status =", response.status);
+        return response.json();
+      })
+      .then((json) => {
+        setProducts(json);
+      })
+      .catch((error) => console.error("Error fetching data:", error));
+  }, []);
+
+  return (
+    <>
+      <h2>Click the button to see the products</h2>
+      <button>to the products page</button>
+      <Home />
+      <ProductList products={products} />
+    </>
+  );
 };
 
 export default App;
