6e1b30c8bc521f296e4a239a397b52f1c82c0187
jacekroszkowiak <jacekroszkowiakdev@gmail.com>
2023-12-09 21:25:58 +0100

    props added to ProductList and sort and filter function merged and improved

--- src/App.tsx
+++ src/App.tsx
@@ -6,49 +6,47 @@ import data from "./db/db.json";
 import { Product } from "./model/model";
 
 const App: React.FC = () => {
-  useEffect(() => {
-    fetch("https://jsonplaceholder.typicode.com/posts", {
-      method: "POST",
-      body: JSON.stringify(data),
-      headers: {
-        "Content-type": "application/json; charset=UTF-8",
-      },
-    })
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error("Failed to post data");
-        }
-        return response.json();
-      })
-      .then((json) => console.log("data posted:", json))
-      .catch((error) => console.error("Error posting data:", error));
-  }, []);
+    useEffect(() => {
+        fetch("https://jsonplaceholder.typicode.com/posts", {
+            method: "POST",
+            body: JSON.stringify(data),
+            headers: {
+                "Content-type": "application/json; charset=UTF-8",
+            },
+        })
+            .then((response) => {
+                if (!response.ok) {
+                    throw new Error("Failed to post data");
+                }
+                return response.json();
+            })
+            .catch((error) => console.error("Error posting data:", error));
+    }, []);
 
-  const [products, setProducts] = useState<Product[]>([]);
+    const [products, setProducts] = useState<Product[]>([]);
 
-  useEffect(() => {
-    fetch("http://localhost:8089/products")
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error("Failed to fetch data");
-        }
-        console.log("response.status =", response.status);
-        return response.json();
-      })
-      .then((json) => {
-        setProducts(json);
-      })
-      .catch((error) => console.error("Error fetching data:", error));
-  }, []);
+    useEffect(() => {
+        fetch("http://localhost:3000/products")
+            .then((response) => {
+                if (!response.ok) {
+                    throw new Error("Failed to fetch data");
+                }
+                return response.json();
+            })
+            .then((json) => {
+                setProducts(json);
+            })
+            .catch((error) => console.error("Error fetching data:", error));
+    }, []);
 
-  return (
-    <>
-      <h2>Click the button to see the products</h2>
-      <button>to the products page</button>
-      <Home />
-      <ProductList products={products} />
-    </>
-  );
+    return (
+        <>
+            <h2>Click the button to see the products</h2>
+            <button>to the products page</button>
+            <Home />
+            <ProductList products={products} />
+        </>
+    );
 };
 
 export default App;
--- src/components/Products.component.tsx
+++ src/components/Products.component.tsx
@@ -3,190 +3,210 @@ import { useEffect, useState } from "react";
 import { Product } from "../model/model";
 
 export const ProductList: React.FC<{ products: Product[] }> = ({
-  products,
+    products,
 }) => {
-  const [uniqueManufacturers, setUniqueManufacturers] = useState<Product[]>([]);
-  const [uniqueModels, setUniqueModels] = useState<Product[]>([]);
-  const [uniqueYears, setUniqueYears] = useState<Product[]>([]);
-  const [sorted, setSorted] = useState<Product[]>([]);
-  const [filterProperty, setFilterProperty] = useState<"" | keyof Product>("");
-  const [filtered, setFiltered] = useState<Product[]>([]);
-
-  useEffect(() => {
-    const uniqueManufacturers = getFilteredAndSortedProducts(
-      products,
-      (product) => product.productName,
-      "productName"
+    const [uniqueManufacturers, setUniqueManufacturers] = useState<Product[]>(
+        []
     );
-
-    setUniqueManufacturers(uniqueManufacturers);
-
-    const uniqueModels = getFilteredAndSortedProducts(
-      products,
-      (product) => product.model,
-      "model"
+    const [uniqueModels, setUniqueModels] = useState<Product[]>([]);
+    const [uniqueYears, setUniqueYears] = useState<Product[]>([]);
+    const [sorted, setSorted] = useState<Product[]>([]);
+    const [filterProperty, setFilterProperty] = useState<"" | keyof Product>(
+        ""
     );
+    const [filtered, setFiltered] = useState<Product[]>([]);
 
-    setUniqueModels(uniqueModels);
+    useEffect(() => {
+        if (products) {
+            const uniqueManufacturers = getFilteredAndSortedProducts(
+                products,
+                (product) => product.productName,
+                "productName"
+            );
 
-    const uniqueYears = getFilteredAndSortedProducts(
-      products,
-      (product) => product.year,
-      "year"
-    );
+            setUniqueManufacturers(uniqueManufacturers);
 
-    setUniqueYears(uniqueYears);
-  }, [products]);
+            const uniqueModels = getFilteredAndSortedProducts(
+                products,
+                (product) => product.model,
+                "model"
+            );
 
-  const getFilteredAndSortedProducts = (
-    products: Product[],
-    key: (product: Product) => string | number,
-    productKey: "productName" | "model" | "year"
-  ): Product[] => {
-    const uniqueProducts = Array.from(
-      new Map(products.map((product) => [key(product), product])).values()
-    );
+            setUniqueModels(uniqueModels);
 
-    uniqueProducts.sort((a, b) => {
-      if (typeof productKey === "string") {
-        return (a[productKey] as string).localeCompare(b[productKey] as string);
-      }
+            const uniqueYears = getFilteredAndSortedProducts(
+                products,
+                (product) => product.year,
+                "year"
+            );
 
-      if (typeof productKey === "number") {
-        return a.year - b.year;
-      }
-      return 1;
-    });
+            setUniqueYears(uniqueYears);
+        }
+    }, [products]);
 
-    return uniqueProducts;
-  };
+    const getFilteredAndSortedProducts = (
+        products: Product[],
+        key: (product: Product) => string | number,
+        productKey: "productName" | "model" | "year"
+    ): Product[] => {
+        const uniqueProducts = Array.from(
+            new Map(products.map((product) => [key(product), product])).values()
+        );
 
-  const handleSort = () => {
-    const sortedProducts = products.toSorted((a, b) =>
-      a.productName.localeCompare(b.productName)
-    );
-    setSorted(sortedProducts);
-    setFiltered([]);
-  };
+        uniqueProducts.sort((a, b) => {
+            if (typeof a[productKey] === "string") {
+                return (a[productKey] as string).localeCompare(
+                    b[productKey] as string
+                );
+            }
 
-  const handleFilter = (filterProperty: string | number) => {
-    // Check if the value is present in the object
-    if (filterProperty === "") {
-      return;
-    }
-    if (products.some((item) => Object.values(item).includes(filterProperty))) {
-      const filteredProducts = products.filter((item) =>
-        Object.values(item).includes(filterProperty)
-      );
-      setFiltered(filteredProducts);
-      setSorted([]);
-    }
-  };
+            if (typeof a[productKey] === "number") {
+                return a.year - b.year;
+            }
+            return 1;
+        });
 
-  const handleFilterPropertyChange = (
-    event: React.ChangeEvent<HTMLSelectElement>
-  ) => {
-    console.log(event.target.value);
-    setFilterProperty(event.target.value as keyof Product);
-  };
+        return uniqueProducts;
+    };
 
-  return (
-    <>
-      <h3>The Products:</h3>
-      {/* Render all products form the data base on page load */}
-      <div className="products-container">
-        {sorted.length === 0 && filtered.length === 0 && (
-          <div>
-            {products.map((product) => (
-              <div key={product.id}>
-                <strong>{product.productName}</strong> - {product.model} (
-                {product.year})
-              </div>
-            ))}
-          </div>
-        )}
-      </div>
+    const handleSort = () => {
+        const sortedProducts = products.toSorted((a, b) =>
+            a.productName.localeCompare(b.productName)
+        );
+        setSorted(sortedProducts);
+        setFiltered([]);
+    };
 
-      {/* conditionally render the sorted  products by clicking button: */}
-      <button onClick={handleSort}>sort products a - z</button>
+    const handleFilter = (filterProperty: string | number) => {
+        // Check if the value is present in the object
+        if (filterProperty === "") {
+            return;
+        }
+        if (
+            products.some((item) =>
+                Object.values(item).includes(filterProperty)
+            )
+        ) {
+            const filteredProducts = products.filter((item) =>
+                Object.values(item).includes(filterProperty)
+            );
+            setFiltered(filteredProducts);
+            setSorted([]);
+        }
+    };
 
-      {/* conditionally render the filtered products by clicking button: */}
-      <button onClick={() => handleFilter(filterProperty)}>
-        Filter by model:
-      </button>
-      <label>
-        <select value={filterProperty} onChange={handleFilterPropertyChange}>
-          <option value="">-- Select Property --</option>
-          {uniqueModels.map((product) => (
-            <option key={product.id} value={product.model}>
-              {product.model}
-            </option>
-          ))}
-        </select>
-      </label>
+    const handleFilterPropertyChange = (
+        event: React.ChangeEvent<HTMLSelectElement>
+    ) => {
+        setFilterProperty(event.target.value as keyof Product);
+    };
 
-      <button onClick={() => handleFilter(filterProperty)}>
-        Filter Manufacturer
-      </button>
-      <label>
-        <select value={filterProperty} onChange={handleFilterPropertyChange}>
-          <option value="">-- Select Property --</option>
-          {uniqueManufacturers.map((product) => (
-            <option key={product.id} value={product.productName}>
-              {product.productName}
-            </option>
-          ))}
-        </select>
-      </label>
-
-      {/* //double tilde to transform property into Number */}
-      <button onClick={() => handleFilter(~~filterProperty)}>
-        Filter Year {filterProperty}
-      </button>
-      <label>
-        <select value={filterProperty} onChange={handleFilterPropertyChange}>
-          <option value="">-- Select Property --</option>
-          {uniqueYears.map((product) => (
-            <option key={product.id} value={product.year}>
-              {product.year}
-            </option>
-          ))}
-        </select>
-      </label>
-
-      <div className="products-container">
-        {filtered.map((product) => (
-          <div className="product-card" key={product.id}>
-            <div className="product-image-container">
-              <img
-                className="product-image"
-                src={`../../public/images/${product.model}.jpg`}
-                alt={`image of ${product.productName} ${product.model}`}
-              />
+    return (
+        <>
+            <h3>The Products:</h3>
+            {/* Render all products form the data base on page load */}
+            <div className="products-container">
+                {sorted.length === 0 && filtered.length === 0 && (
+                    <div>
+                        {products.map((product) => (
+                            <div key={product.id}>
+                                <strong>{product.productName}</strong> -{" "}
+                                {product.model} ({product.year})
+                            </div>
+                        ))}
+                    </div>
+                )}
             </div>
-            <strong>{product.productName}</strong>
-            <p>
-              {product.model} {product.year}
-            </p>
-          </div>
-        ))}
 
-        {sorted.map((product) => (
-          <div className="product-card" key={product.id}>
-            <div className="product-image-container">
-              <img
-                className="product-image"
-                src={`../../public/images/${product.model}.jpg`}
-                alt={`image of ${product.productName} ${product.model}`}
-              />
+            {/* conditionally render the sorted  products by clicking button: */}
+            <button onClick={handleSort}>sort products a - z</button>
+
+            {/* conditionally render the filtered products by clicking button: */}
+            <button onClick={() => handleFilter(filterProperty)}>
+                Filter by model:
+            </button>
+            <label>
+                <select
+                    value={filterProperty}
+                    onChange={handleFilterPropertyChange}
+                >
+                    <option value="">-- Select Property --</option>
+                    {uniqueModels.map((product) => (
+                        <option key={product.id} value={product.model}>
+                            {product.model}
+                        </option>
+                    ))}
+                </select>
+            </label>
+
+            <button onClick={() => handleFilter(filterProperty)}>
+                Filter Manufacturer
+            </button>
+            <label>
+                <select
+                    value={filterProperty}
+                    onChange={handleFilterPropertyChange}
+                >
+                    <option value="">-- Select Property --</option>
+                    {uniqueManufacturers.map((product) => (
+                        <option key={product.id} value={product.productName}>
+                            {product.productName}
+                        </option>
+                    ))}
+                </select>
+            </label>
+
+            {/* //double tilde to transform property into Number */}
+            <button onClick={() => handleFilter(~~filterProperty)}>
+                Filter Year {filterProperty}
+            </button>
+            <label>
+                <select
+                    value={filterProperty}
+                    onChange={handleFilterPropertyChange}
+                >
+                    <option value="">-- Select Property --</option>
+                    {uniqueYears.map((product) => (
+                        <option key={product.id} value={product.year}>
+                            {product.year}
+                        </option>
+                    ))}
+                </select>
+            </label>
+
+            <div className="products-container">
+                {filtered.map((product) => (
+                    <div className="product-card" key={product.id}>
+                        <div className="product-image-container">
+                            <img
+                                className="product-image"
+                                src={`../../public/images/${product.model}.jpg`}
+                                alt={`image of ${product.productName} ${product.model}`}
+                            />
+                        </div>
+                        <strong>{product.productName}</strong>
+                        <p>
+                            {product.model} {product.year}
+                        </p>
+                    </div>
+                ))}
+
+                {sorted.map((product) => (
+                    <div className="product-card" key={product.id}>
+                        <div className="product-image-container">
+                            <img
+                                className="product-image"
+                                src={`../../public/images/${product.model}.jpg`}
+                                alt={`image of ${product.productName} ${product.model}`}
+                            />
+                        </div>
+                        <strong>{product.productName}</strong>
+                        <p>
+                            {product.model} {product.year}
+                        </p>
+                    </div>
+                ))}
             </div>
-            <strong>{product.productName}</strong>
-            <p>
-              {product.model} {product.year}
-            </p>
-          </div>
-        ))}
-      </div>
-    </>
-  );
+        </>
+    );
 };
